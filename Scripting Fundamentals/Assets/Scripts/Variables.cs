9e9121e32a9ff722d11434c0ab24e2db77349281
b3east1 <rhylendelarosa@gmail.com>
2023-10-03 12:01:11 -0700

    Update files

---
+++ Scripting Fundamentals/Assets/Scripts/Variables.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Public Memeber/Gobal Variables
+    public float MoveSpeed = 10f;
+    public int PlayerScore = 0;
+    public string PlayerName = "John Voe";
+    public bool IsPlayerActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
